929a6b729e92f33dd791ca386875037de07d3701
Visiona <darbis@gmail.com>
2017-08-20 12:09:39 +0100

    Created form and new route drop_piece

--- app/controllers/connect_four_controller.rb
+++ app/controllers/connect_four_controller.rb
@@ -3,5 +3,13 @@ class ConnectFourController < ApplicationController
   def new
 
   end
-  
+
+  def show
+
+  end
+
+  def drop_piece
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "connect_four#new"
 
   resources :connect_four
+  post "connect_four/drop_piece", to: "connect_four#drop_piece"
 
 end
